27ebb929115bb2f4d8b72efb980155bd1e712b14
bashlui <luisboltech005@outlook.com>
2025-08-21 12:42:16 -0600

    Initial Commit

---
+++ nasa-api-practice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  nasa-api-practice
+//
+//  Created by toño on 21/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ nasa-api-practice/nasa_api_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  nasa_api_practiceApp.swift
+//  nasa-api-practice
+//
+//  Created by toño on 21/08/25.
+//
+
+import SwiftUI
+
+@main
+struct nasa_api_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
